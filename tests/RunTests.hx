8c3e95357e6283f897a799d602b54ac7a13fec08
Juraj Kirchheim <back2dos@googlemail.com>
2019-03-10 15:01:33 +0100

    Lixify.

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -4,16 +4,15 @@ import haxe.crypto.Base64;
 import haxe.io.Bytes;
 import haxe.unit.TestRunner;
 
-#if flash
-typedef Sys = flash.system.System;
-#end
 class RunTests { 
 
   static function main() {
     var t = new TestRunner();
     t.add(new TestStringSlice());
-    if (!t.run())
-      Sys.exit(500);
+    travix.Logger.exit(
+      if (t.run()) 0
+      else 500
+    );
   }
   
 }
